a3fb893194b7265804ab12ef4d4c59a8aaea320a
nipuns11 <nipunsharma@nipuns-Mac-mini.local>
2015-10-23 16:49:03 -0400

    a basic image viewer in swift

---
+++ DetailViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DetailViewController.swift
+//  viewImages
+//
+//  Created by nipun sharma on 2015-10-23.
+//  Copyright © 2015 nipun. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+    @IBOutlet weak var detailImageView: UIImageView!
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+        
+    }
+    
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+
+
+}
+
